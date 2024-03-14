1f2e5623dced066dc59aba530a06248846087c8e
Hahyun Baek <hahyun8587@gmail.com>
2024-03-14 15:27:47 +0900

    feat: implement gen_init_s() and simualte()

--- alphasix/alphasixsimulator.py
+++ alphasix/alphasixsimulator.py
@@ -1,91 +1,85 @@
 import numpy as np
 from tools.AlphaZero.alphazero.simulator import Simulator
-from .utils import a_to_locs
+from alphasix.utils import atoc
 
 class AlphaSixSimulator(Simulator):
     """Simulator of alphasix.
+    
+    Sets size of the board and max number of red stones of this instance.
+    
+    The state of alphasix is 2 x `board_size` x `board_size` 
+    3D array where,
+    * The first plane represents stones on the board.
+    * The second plane represents turn of a player. 
+    
+    The first plane of initial state has `0` to `max_reds` number of red stones
+    and `1` black stone on the center. The second plane is filled with 
+    `AlphaSixSimulator.WHITE` to indicate turn of white.
+    
+    Args:
+        board_size (int): The size of the board.
+            It should be odd and bigger than `7`.
+        max_red (int): The max number of red stones.
+            It should be in the range of `[0, 5]`.
     """
     
     #class variables
-    WHITE = -1
-    BLACK = 1
+    BLACK = -1
+    WHITE = 1
     RED = 2
-    _board_size = 19
-    _max_red_stones = 5
     
-    @classmethod
-    def gen_init_s(cls) -> np.ndarray:
-        s = np.zeros((2, cls._board_size, cls._board_size), dtype=np.int32)
-        n_red_stone = np.random.choice(cls._max_red_stones + 1)
-        red_stone_loc = np.random.choice(cls._board_size ** 2, n_red_stone)
+    #instance variables
+    _board_size: int
+    _max_red: int
+    
+    def __init__(self, board_size: int, max_red: int):
+        self._board_size = board_size
+        self._max_red = max_red
+    
+        
+    def gen_init_s(self) -> np.ndarray:
+        rng = np.random.default_rng()
+        stones = np.zeros((self._board_size, self._board_size))
+        
+        for loc in rng.choice(self._board_size * self._board_size, 
+                              size=rng.choice(self._max_red + 1), 
+                              replace=False):
+            stones[loc // self._board_size][loc % self._board_size] \
+                    = AlphaSixSimulator.RED
+        
+        stones[self._board_size // 2][self._board_size // 2] \
+                = AlphaSixSimulator.BLACK
+        
+        turn = np.full((self._board_size, self._board_size), 
+                       AlphaSixSimulator.WHITE, dtype=float)    
+                
+        return np.stack((stones, turn))
+  
+    
+    def simulate(self, s: np.ndarray, a: int) -> tuple:
+        coords = atoc(a, self._board_size)
+        is_invalid_a = False
+        
+        for coord in coords:
+            if s[0][coord[0]][coord[1]] != 0:
+                is_invalid_a = True
+                
+                break
 
-        for loc in red_stone_loc:
-            s[0][loc // cls._board_size][loc % cls._board_size] = cls.RED
+        if is_invalid_a:
+            return (-1, None)
         
-        s[0][9][9] = cls.BLACK
-        s[1].fill(cls.WHITE)
-        
-        return s
-    
-    
-    def simulate(self, s: np.ndarray, a: int) -> np.ndarray:
-        next_s = s.copy()
-        locs = a_to_locs(a)
-        locs[0] = int(locs[0])
-        locs[1] = int(locs[1])
-        
-        for i in range(2):
-            if next_s[0][locs[i] // self._board_size][locs[i] % self._board_size] != 0:
-                return None
-        
-            next_s[0][locs[i] // self._board_size][locs[i] % self._board_size] = s[1][0][0]
+        s_prime = np.array(s)
+         
+        for coord in coords:
+            s_prime[0][coord[0]][coord[1]] = s[1][0][0]
             
-        next_s[1].fill(-s[1][0][0])
+        s_prime[1].fill(-s[1][0][0])
         
-        return next_s
-    
-    
-    def is_terminal(self, s: np.ndarray, a: int) -> bool:
-        direction = [(0, -1), (-1, -1), (-1, 0), (-1, 1)]
-        locs = a_to_locs(a)
+        return (1 if self.is_terminal(s_prime) else 0, s_prime)
         
-        for i in range(2):
-            for j in range(4):
-                cnt = [0, 0, 0, 0]
-                hy = locs[i] // self._board_size
-                hx = locs[i] % self._board_size
-                ty = locs[i] // self._board_size
-                tx = locs[i] % self._board_size
-                dy = direction[j][0]
-                dx = direction[j][1]
-
-                for _ in range(6):
-                    if ty < 0 or ty >= self._board_size or tx < 0 or tx >= self._board_size:
-                        break
-                    
-                    cnt[s[0][ty][tx]] += 1
-                    ty += dy
-                    tx += dx
-
-                ty -= dy
-                tx -= dx
-            
-                for k in range(6):
-                    if cnt[-s[1][0][0]] == 6:
-                        return True
-                    
-                    cnt[s[0][ty][tx]] -= 1
-                    
-                    ty -= dy
-                    tx -= dx
-                    
-                    hy -= dy
-                    hx -= dx
-                    
-                    if hy < 0 or hy >= self._board_size or hx < 0 or hx >= self._board_size:
-                        break
-                    
-                    cnt[s[0][hy][hx]] += 1
-        
-        return False
+       
+    def is_terminal(self, s: np.ndarray) -> bool:
+        None
+                
                    
