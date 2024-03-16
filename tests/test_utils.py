eb017757572918b0e1d4ddf80454cebe1f1cd510
Hahyun Baek <hahyun8587@gmail.com>
2024-03-16 23:58:32 +0900

    feat: support exception assertion

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,14 +4,17 @@ from alphasix.utils import atoc
 class TestUtils(unittest.TestCase):
     
     def test_atoc(self) -> None:
-        """Tests converting action into coordinates.
+        """Tests converting an action into the corresponding coordinates.
         """
         
-        coord1, coord2 = atoc(361, 19)
-        self.assertEqual([*coord1, *coord2], [0, 1, 0, 3])
+        self.assertRaises(ValueError, atoc, -1, 13)
+        self.assertRaises(ValueError, atoc, 64980, 19)
         
-        coord1, coord2 = atoc(1076, 19)
-        self.assertEqual([*coord1, *coord2], [0, 2, 18, 18])
+        coords = atoc(361, 19)
+        self.assertEqual(coords, ((0, 1), (0, 3)))
+        
+        coords = atoc(1076, 19)
+        self.assertEqual(coords, ((0, 2), (18, 18)))
         
         
         
