c5a001b3aeb53794301dffae4e04495007d100b6
Hahyun Baek <hahyun8587@gmail.com>
2024-03-21 23:38:01 +0900

    feat: implement is_terminal()

--- alphasix/alphasixsimulator.py
+++ alphasix/alphasixsimulator.py
@@ -70,16 +70,66 @@ class AlphaSixSimulator(Simulator):
             return (-1, None)
         
         s_prime = np.array(s)
+        color = s[1][0][0]
          
         for coord in coords:
-            s_prime[0][coord[0]][coord[1]] = s[1][0][0]
+            s_prime[0][coord[0]][coord[1]] = color
             
-        s_prime[1].fill(-s[1][0][0])
+        s_prime[1] = -s_prime[1]
         
         return (1 if self.is_terminal(s_prime) else 0, s_prime)
         
        
     def is_terminal(self, s: np.ndarray) -> bool:
-        None
+        if s is None:
+            return True
+    
+        start_idxs = ((0, 0), (0, 0), 
+                      (0, 0), (1, 0), 
+                      (0, self._board_size - 1), (1, self._board_size - 1))     
+        start_dirs = ((1, 0), (0, 1), (0, 1), (1, 0), (0, -1), (1, 0))
+        ht_dirs = ((0, 1), (1, 0), (1, 1), (1, 1), (1, -1), (1, -1))    
+        terminal_sum = 6 * -s[1][0][0]
+        
+        for i in range(6):
+            start_i, start_j = start_idxs[i]
+         
+            while (start_i < self._board_size 
+                   and 0 <= start_j < self._board_size):
+                start_idx = (start_i, start_j)
+                head_i, head_j = start_idx
+                tail_i, tail_j = start_idx
+                sum = 0
                 
-                   
+                for j in range(1, 7):
+                    sum += s[0][head_i][head_j]
+                    head_i += ht_dirs[i][0]
+                    head_j += ht_dirs[i][1]
+                    
+                    if (head_i >= self._board_size 
+                            or head_j < 0 or head_j >= self._board_size):
+                        break
+                
+                if j == 6 and sum == terminal_sum:
+                    return True
+                
+                while (head_i < self._board_size 
+                       and 0 <= head_j < self._board_size):
+                    sum -= s[0][tail_i][tail_j]
+                    tail_i += ht_dirs[i][0]
+                    tail_j += ht_dirs[i][1]
+                    
+                    sum += s[0][head_i][head_j]
+                    head_i += ht_dirs[i][0]
+                    head_j += ht_dirs[i][1]
+                    
+                    if sum == terminal_sum:
+                        return True 
+                
+                start_i += start_dirs[i][0]
+                start_j += start_dirs[i][1]
+            
+        return False                       
+                
+                
+                            
