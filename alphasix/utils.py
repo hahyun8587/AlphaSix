9e0afa0318565af6b9947f9082838f5a21bb1606
HahyunBaek <hahyun8587@gmail.com>
2024-05-13 22:04:53 +0900

    fix: fix convertion equation and element type of returned nested tuple

--- alphasix/utils.py
+++ alphasix/utils.py
@@ -17,9 +17,10 @@ def atoc(a: int, n: int) -> tuple:
     
     if a < 0 or a > n * n * (n * n - 1) / 2 - 1:
         raise ValueError('action out of range.')
-    
-    i = floor((2 * n * n - 1 - sqrt((2 * n * n - 1) ** 2 - 8 * a)) / 2 + 1)   
-    locs = [i - 1, a + ((i - 1) ** 2 - (2 * n * n - 1) * (i - 1)) / 2 + i]
+
+    i = floor((2 * n * n + 1 
+               - sqrt((2 * n * n + 1) ** 2 -  4 * (2 * n * n + 2 * a))) / 2)    
+    locs = [i - 1, a + 1 + i * (i + 1) // 2 - 1 + (i - 1) * -n * n]
 
     return tuple((loc // n, loc % n) for loc in locs) 
 
